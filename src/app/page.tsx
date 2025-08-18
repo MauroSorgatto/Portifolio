a1c83573dd99a5fc1ff531b0eed5c0498777bb27
root <mauro.sorgatto@azzero.tec.br>
2025-08-18 10:17:34 -0300

    fix: cv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,7 +144,7 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="https://drive.google.com/file/d/1N1LHVLiH9HCPOL1DuCx-OoSwoImrKn7b/view?usp=drive_link" target='blank' variant="secondary" className="group mt-6 w-full">
+      <Button href="https://drive.google.com/file/d/19Ypiz-xd-XhZbYg89bafqtJIRKu2j-j5/view" target='blank' variant="secondary" className="group mt-6 w-full">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
